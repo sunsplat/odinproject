a26f6178e939df237fc08995494aa5de54e03be2
sunsplat <heeycutie@yahoo.com>
2015-07-26 05:34:59 +0800

    second commit add method

---
+++ cipher.rb
@@ -0,0 +1,14 @@
+puts "Word please: "
+text = gets.chomp
+
+puts "Number 1-5 please: "
+num = gets.chomp
+
+def caesar_cipher(text, num)
+	letters = text.chars.to_a
+	letters.each { |word| }
+	puts 
+
+
+
+caesar_cipher("method", 5)
