cbdf98b8007ef7262476271215b3f3b218d0e05b
sunsplat <heeycutie@yahoo.com>
2015-07-26 09:54:53 +0800

    saturday last commit

--- cipher.rb
+++ cipher.rb
@@ -2,13 +2,13 @@ puts "Word please: "
 text = gets.chomp.downcase
 
 puts "Number 1-10 please: "
-num = gets.chomp.to_i
+n = gets.chomp.to_i
 
 
 def caesar_cipher(text, n)
 	alphabet = [*('a'..'z')]
+	alphabet.cycle(2)
 	text.tr('a-z', alphabet[n..n+26].join)
-
 end
 
-print caesar_cipher(text, num)
+puts caesar_cipher(text, n)
