2117f6991509dbd30f5e70a0a3eda1b1e23a48ad
sunsplat <heeycutie@yahoo.com>
2015-07-27 06:58:37 +0800

    solution

--- cipher.rb
+++ cipher.rb
@@ -1,14 +1,15 @@
 puts "Word please: "
 text = gets.chomp.downcase
 
-puts "Number 1-10 please: "
+puts "Number please: "
 n = gets.chomp.to_i
 
 
 def caesar_cipher(text, n)
-	alphabet = [*('a'..'z')]
-	alphabet.cycle(2)
-	text.tr('a-z', alphabet[n..n+26].join)
+	alphabet = ('a'..'z').to_a # turns each letter into its own array
+	key = Hash[alphabet.zip(alphabet.rotate(n))] 
+	# key takes the alphabet and repeats it depending on the number n
+	text.each_char.inject("") { |encrypted, char| encrypted + key[char] }
 end
 
 puts caesar_cipher(text, n)
