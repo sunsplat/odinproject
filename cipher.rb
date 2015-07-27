56d1ceb7736dc1b556408f6456bf0ffc9c3318f1
sunsplat <heeycutie@yahoo.com>
2015-07-28 02:01:08 +0800

    removed comment

--- cipher.rb
+++ cipher.rb
@@ -8,9 +8,7 @@ n = gets.chomp.to_i
 def caesar_cipher(text, n)
 	alphabet = ('a'..'z').to_a # turns each letter into its own array
 	key = Hash[alphabet.zip(alphabet.rotate(n))] 
-	# key takes the alphabet and repeats it depending on the number n
 	text.each_char.inject("") { |newtext, char| newtext + key[char] }
-
 end
 
 puts caesar_cipher(text, n)
